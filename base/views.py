4135e9647843b6118881373ad5f290e8a8955797
Anthony Lancaster <ajl6yc@virginia.edu>
2020-02-19 09:18:44 -0500

    Migrate files from site one

--- base/views.py
+++ base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# error_404 and error_500 set up error custom error pages
+#def error_404(request, exception):
+#	data={}
+#	return render(request, '404.html', data)
+
+#def error_500(request):
+#	return render(request, '500.html')
+
+
+def index(request):
+    return render(request, 'index.html')
